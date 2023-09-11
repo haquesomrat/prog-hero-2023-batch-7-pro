072294ee5b7b1487ea94267c887acd71e52f64f6
haquesomrat <ihsomrat.official@gmail.com>
2023-09-12 00:21:31 +0600

    legal site done

--- justice-website/tailwind.config.js
+++ justice-website/tailwind.config.js
@@ -1,9 +1,26 @@
-/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["*"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        garamond: ["EB Garamond", ...defaultTheme.fontFamily.serif],
+        poppins: ["Poppins", ...defaultTheme.fontFamily.serif],
+      },
+      colors: {
+        gold: "#B68C5A",
+        black: "#414040",
+        "light-black": "#707070",
+      },
+      spacing: {
+        4.5: "18px",
+        7.5: "30px",
+      },
+      fontSize: {
+        "2.5rem": "40px",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
